15b8a50adf2ad67725e903a399af93339884f105
elopez73 <esaulopez14@gmail.com>
2023-08-10 12:45:05 -0700

    Feat: New BoxScore Challenge

---
+++ src/components/Mlb.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../stlying/Mlb.css';
+const apiBaseUrl = 'http://localhost:3001';
+function Mlb() {
+
+    const [awayteam, setAwayTeam] = useState(null);
+    const [awaybatters, setAwayBatters] = useState(null);
+    const [awayfielding, setAwayFielding] = useState(null);
+    const [awaypitchers, setAwayPitchers] = useState(null);
+    const [hometeam, setHomeTeam] = useState(null);
+    const [homebatters, setHomeBatters] = useState(null);
+    const [homefielding, setHomeFielding] = useState(null);
+    const [homepitchers, setHomePitchers] = useState(null);
+    const [gameData, setGameData] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${apiBaseUrl}/api/feed/mlb`);
+                console.log('API response data:', response);
+                setGameData(response.data);
+                setAwayTeam(response.data.gameData.away_team.full_name);
+                setAwayBatters(response.data.gameData.away_batters);
+                setAwayFielding(response.data.gameData.away_fielding);
+                setAwayPitchers(response.data.gameData.away_pitchers);
+                setHomeTeam(response.data.gameData.home_team.full_name);
+                setHomeBatters(response.data.gameData.home_batters);
+                setHomeFielding(response.data.gameData.home_fielding);
+                setHomePitchers(response.data.gameData.home_pitchers);
+                console.log(response.data.gameData);
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
+
+    if (!gameData) {
+        return <div>Loading...</div>;
+    }
+    return (
+        <div> <h2>Game Details</h2>
+            <h3>{awayteam} vs {hometeam}</h3>
+            <div className="baseball-game">
+                <div className="sections" >
+                    <h5>Away </h5>
+                    <h4> {awayteam}</h4>
+                    <div className="team">
+                        <div className="team-sections">
+                            <ul>
+                                <li>
+                                    <h1 className="Batters">Batters</h1>
+                                    <span className="Batters" style={{ fontWeight: 'bold' }}>AVG</span>
+                                    <span className="Batters" style={{ fontWeight: 'bold' }}>OBP</span>
+                                    <span className="Batters" style={{ fontWeight: 'bold' }}>OPS </span>
+                                </li>
+
+                                {awaybatters.map((batter, index) => {
+                                    const anameParts = batter.display_name.split(' '); // Split the name into parts
+                                    const afirstInitial = anameParts[0].charAt(0).toUpperCase();
+                                    const alastName = anameParts[1];
+
+                                    return (
+                                        <li key={index}>
+                                            <h1 className="Batters">{`${afirstInitial}.${alastName}`}</h1>
+                                            <span className="Batters">{batter.avg}</span>
+                                            <span className="Batters">{batter.obp}</span>
+                                            <span className="Batters">{batter.ops}</span>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                        <div className="team-sections">
+                            <ul>
+                                <li>
+                                    <h1 className="Fielders">Fielders</h1>
+                                    <span className="Fielders" style={{ fontWeight: 'bold' }}>Errors</span>
+
+                                </li>
+
+                                {awayfielding.map((fielder, index) => {
+                                    const nameParts = fielder.display_name.split(' '); // Split the name into parts
+                                    const firstInitial = nameParts[0].charAt(0).toUpperCase();
+                                    const lastName = nameParts[1];
+
+                                    return (
+                                        <li key={index}>
+                                            <h1 className="Fielders">{`${firstInitial}.${lastName}`}</h1>
+                                            <span className="Fielders">{fielder.errors}</span>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                        <div className="team-sections">
+                            <ul>
+                                <li>
+                                    <h1 className="Pitchers">Pitchers</h1>
+                                    <span className="Pitchers" style={{ fontWeight: 'bold' }}>ERA</span>
+                                    <span className="Pitchers" style={{ fontWeight: 'bold' }}>Strikeouts</span>
+                                </li>
+
+                                {awaypitchers.map((pitcher, index) => {
+                                    const nameParts = pitcher.display_name.split(' '); // Split the name into parts
+                                    const firstInitial = nameParts[0].charAt(0).toUpperCase();
+                                    const lastName = nameParts[1];
+
+                                    return (
+                                        <li key={index}>
+                                            <h1 className="Pitchers">{`${firstInitial}.${lastName}`}</h1>
+                                            <span className="Pitchers">{pitcher.era}</span>
+                                            <span className="Pitchers">{pitcher.strike_outs}</span>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="sections">
+                    <h5>Home</h5>
+                    <h4>{hometeam}</h4>
+                    <div className="team">
+                        <div className="team-sections">
+                            <ul>
+                                <li>
+                                    <h1 className="Batters">Batters</h1>
+                                    <span className="Batters"  style={{fontWeight:'bold'}}>AVG</span>
+                                    <span className="Batters"  style={{ fontWeight: 'bold' }}>OBP</span>
+                                    <span className="Batters" style={{ fontWeight: 'bold' }}>OPS </span>
+                                </li>
+
+                                {homebatters.map((batter, index) => {
+                                    const nameParts = batter.display_name.split(' '); // Split the name into parts
+                                    const firstInitial = nameParts[0].charAt(0).toUpperCase();
+                                    const lastName = nameParts[1];
+
+                                    return (
+                                        <li key={index}>
+                                            <h1 className="Batters">{`${firstInitial}.${lastName}`}</h1>
+                                            <span className="Batters">{batter.avg}</span>
+                                            <span className="Batters">{batter.obp}</span>
+                                            <span className="Batters">{batter.ops}</span>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                        <div className="team-sections">
+                            <ul>
+                                <li>
+                                    <h1 className="Fielders">Fielders</h1>
+                                    <span className="Fielders" style={{ fontWeight: 'bold' }}>Errors</span>
+
+                                </li>
+
+                                {homefielding.map((fielder, index) => {
+                                    const nameParts = fielder.display_name.split(' '); // Split the name into parts
+                                    const firstInitial = nameParts[0].charAt(0).toUpperCase();
+                                    const lastName = nameParts[1];
+
+                                    return (
+                                        <li key={index}>
+                                            <h1 className="Fielders">{`${firstInitial}.${lastName}`}</h1>
+                                            <span className="Fielders">{fielder.errors}</span>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                        <div className="team-sections">
+                            <ul>
+                                <li>
+                                    <h1 className="Pitchers">Pitchers</h1>
+                                    <span className="Pitchers" style={{ fontWeight: 'bold' }}>ERA</span>
+                                    <span className="Pitchers" style={{ fontWeight: 'bold' }}>Strikeouts</span>
+                                </li>
+
+                                {homepitchers.map((pitcher, index) => {
+                                    const nameParts = pitcher.display_name.split(' '); // Split the name into parts
+                                    const firstInitial = nameParts[0].charAt(0).toUpperCase();
+                                    const lastName = nameParts[1];
+
+                                    return (
+                                        <li key={index}>
+                                            <h1 className="Pitchers">{`${firstInitial}.${lastName}`}</h1>
+                                            <span className="Pitchers">{pitcher.era}</span>
+                                            <span className="Pitchers">{pitcher.strike_outs}</span>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Mlb;
---
+++ src/components/Nba.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../stlying/Nba.css'
+const apiBaseUrl = 'http://localhost:3001';
+function Nba() {
+    const [gameData, setGameData] = useState(null);
+    const [awayteam, setAwayTeam] = useState(null);
+    const [awaystats, setStats] = useState(null);
+    const [hometeam, setHomeTeam] = useState(null);
+    const [homestats, setHomeStats] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${apiBaseUrl}/api/feed/nba`);
+                console.log('API response data:', response.data);
+                setGameData(response.data.gameData);
+                setAwayTeam(response.data.gameData.away_team.full_name);
+                setStats(response.data.gameData.away_stats);
+                setHomeTeam(response.data.gameData.home_team.full_name);
+                setHomeStats(response.data.gameData.home_stats);
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (!gameData) {
+        return <div>Loading...</div>;
+    }
+
+
+    return (<div className="contain">
+        <h2>Game Details</h2>
+        <h3>{awayteam} vs {hometeam}</h3>
+        <div className="nba-game">
+
+            <div className="sections">
+                <div className="team">
+                    <div className="team-sections">
+                        <h5>Away </h5>
+                        <h3>{awayteam}</h3>
+                        <ul>
+                            <li>
+                                <h1>Players</h1>
+                                <span style={{ fontWeight: 'bold' }}>Points</span>
+                            </li>
+
+                            {awaystats.map((player, index) => {
+                                const anameParts = player.display_name.split(' '); // Split the name into parts
+                                const afirstInitial = anameParts[0].charAt(0).toUpperCase();
+                                const alastName = anameParts[1];
+
+                                return (
+                                    <li key={index}>
+                                        <h1>{`${afirstInitial}.${alastName}`}</h1>
+                                        <span >{player.points}</span>
+
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="sections">
+                <div className="team">
+                    <div className="team-sections">
+                        <h5>Away </h5>
+                        <h3> {hometeam}</h3>
+                        <ul>
+                            <li>
+                                <h1>Players</h1>
+                                <span style={{ fontWeight: 'bold' }}>Points</span>
+                            </li>
+
+                            {homestats.map((player, index) => {
+                                const anameParts = player.display_name.split(' '); // Split the name into parts
+                                const afirstInitial = anameParts[0].charAt(0).toUpperCase();
+                                const alastName = anameParts[1];
+
+                                return (
+                                    <li key={index}>
+                                        <h1>{`${afirstInitial}.${alastName}`}</h1>
+                                        <span >{player.points}</span>
+
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Nba;
